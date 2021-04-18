cf31dc1c29a7121d2c26cdac021d80c72c4c9475
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-04-18 22:33:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width: 400,
+    height: 300,
+    image_format: 'png',
+    png_quality: 100,
+    dest_width: 380,
+    dest_height: 280
+});
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+function capture() {
+    Webcam.snap(
+        function(image_url) {
+            document.getElementById("result").innerHTML = "<img id='captured_image' src='"+image_url+"'>";
+        }
+    );
+}
+console.log("ml5 version: ", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R_3JGtLfr/model.json", modelLoaded);
+function modelLoaded() {
+    console.log("Model Loaded!")
+}
+function identify() {
+    new_img = document.getElementById("captured_image");
+    classifier.classify(new_img, getResult);
+}
+function getResult(error, results) {
+    if (error) {
+        console.error();
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("accuracy_percentage").innerHTML = (results[0].confidence * 100).toFixed(3)  + " %";
+    }
+}
